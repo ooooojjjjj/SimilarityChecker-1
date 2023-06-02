ea5612a5156043616a7ffe281d6903a5ac9158ef
oj <ozoppp@gmail.com>
2023-06-02 14:31:48 +0900

    Alpha Point 리팩토링

--- TDD_StringSimilarityChecker/StringSimilarityChecker.cpp
+++ TDD_StringSimilarityChecker/StringSimilarityChecker.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+#define MAX_ALPHABET_COUNT (26)
+
 class StringSmilarityChecker
 {
 public:
@@ -43,42 +45,6 @@ public:
         return CalculateLengthPoint();
     }
 
-    int GetAlphaPoint(string sString1, string sString2)
-    {
-        bool anAlphbetMap1[26] = { 0 };
-        bool anAlphbetMap2[26] = { 0 };
-        int nSameCount = 0;
-        int nTotalCount = 0;
-
-        for (int i = 0; i < sString1.size(); i++)
-        {
-            anAlphbetMap1[sString1[i] - 'A'] = true;
-        }
-
-        for (int i = 0; i < sString2.size(); i++)
-        {
-            anAlphbetMap2[sString2[i] - 'A'] = true;
-        }
-
-        for (int i = 0; i < 26; i++)
-        {
-            if (anAlphbetMap1[i] && anAlphbetMap2[i])
-            {
-                nSameCount++;
-            }
-        }
-
-        for (int i = 0; i < 26; i++)
-        {
-            if (anAlphbetMap1[i] || anAlphbetMap2[i])
-            {
-                nTotalCount++;
-            }
-        }
-
-        return 40 * nSameCount / nTotalCount;
-    }
-
     void CompareLength(string sString1, string sString2)
     {
         if (sString1.size() > sString2.size())
@@ -101,9 +67,54 @@ public:
         return (nShortLength - nDiffLength) * MAX_LENGTH_POINT / nShortLength;
     }
 
+    int GetAlphaPoint(string sString1, string sString2)
+    {
+        CompareAlpha(sString1, sString2);
+
+        return CalculateAlphaPoint();
+    }
+
+    void CompareAlpha(string sString1, string sString2)
+    {
+        bool anAlphbetMap1[MAX_ALPHABET_COUNT] = { 0 };
+        bool anAlphbetMap2[MAX_ALPHABET_COUNT] = { 0 };
+
+        for (int i = 0; i < sString1.size(); i++)
+        {
+            anAlphbetMap1[sString1[i] - 'A'] = true;
+        }
+
+        for (int i = 0; i < sString2.size(); i++)
+        {
+            anAlphbetMap2[sString2[i] - 'A'] = true;
+        }
+
+        for (int i = 0; i < MAX_ALPHABET_COUNT; i++)
+        {
+            if (anAlphbetMap1[i] && anAlphbetMap2[i])
+            {
+                nSameCount++;
+            }
+
+            if (anAlphbetMap1[i] || anAlphbetMap2[i])
+            {
+                nTotalCount++;
+            }
+        }
+    }
+
+    int CalculateAlphaPoint()
+    {
+        return MAX_ALPHA_POINT * nSameCount / nTotalCount;
+    }
+
 private:
     const int MAX_LENGTH_POINT = 60;
     int nLongLength = 0;
     int nShortLength = 0;
     int nDiffLength = 0;
+
+    const int MAX_ALPHA_POINT = 40;
+    int nSameCount = 0;
+    int nTotalCount = 0;
 };
