801c2b4093308dfa569c216f356acd08e8863d63
oj <ozoppp@gmail.com>
2023-06-02 12:39:43 +0900

    Length Point 리팩토링

--- TDD_StringSimilarityChecker/StringSimilarityChecker.cpp
+++ TDD_StringSimilarityChecker/StringSimilarityChecker.cpp
@@ -11,10 +11,10 @@ public:
         if (false == CheckInputValidity(sString1, sString2))
             throw invalid_argument("값을 넣어야 합니다.");
 
-        return 0;
+        return GetLengthPoint(sString1, sString2);
     }
 
-    bool CheckInputValidity(string& sString1, string& sString2)
+    bool CheckInputValidity(string sString1, string sString2)
     {
         if (0 == sString1.size() || 0 == sString2.size())
             return false;
@@ -22,10 +22,13 @@ public:
 
     int GetLengthPoint(string sString1, string sString2)
     {
-        int nLengthPoint = 0;
+        CompareLength(sString1, sString2);
 
-        int nLongLength = 0, nShortLength = 0;
+        return CalculateLengthPoint();
+    }
 
+    void CompareLength(string sString1, string sString2)
+    {
         if (sString1.size() > sString2.size())
         {
             nLongLength = sString1.size();
@@ -37,10 +40,17 @@ public:
             nShortLength = sString1.size();
         }
 
-        int nDiffLength = nLongLength - nShortLength;
-
-        nLengthPoint = (nShortLength - nDiffLength) * 60 / nShortLength;
-
-        return nLengthPoint;
+        nDiffLength = nLongLength - nShortLength;
     }
+
+    int CalculateLengthPoint()
+    {
+        return (nShortLength - nDiffLength) * MAX_LENGTH_POINT / nShortLength;
+    }
+
+private:
+    const int MAX_LENGTH_POINT = 60;
+    int nLongLength = 0;
+    int nShortLength = 0;
+    int nDiffLength = 0;
 };
