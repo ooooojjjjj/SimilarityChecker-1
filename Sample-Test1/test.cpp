f674c5cede08ca1bd84280119f0ef284ce3c0810
oj <ozoppp@gmail.com>
2023-06-02 12:02:50 +0900

    Get Total Point 구현

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,35 @@
 #include "pch.h"
 #include "../TDD_StringSimilarityChecker/StringSimilarityChecker.cpp"
+#include <string>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
+using namespace std;
+
+TEST(TestCaseName, GetTotalPoint_NoString1) 
+{
+    StringSmilarityChecker cChecker;
+
+    string sString1 = "";
+    string sString2 = "qwer";
+    
+    EXPECT_THROW(cChecker.GetTotalPoint(sString1, sString2), invalid_argument);
+}
+
+TEST(TestCaseName, GetTotalPoint_NoString2)
+{
+    StringSmilarityChecker cChecker;
+
+    string sString1 = "asdf";
+    string sString2 = "";
+
+    EXPECT_THROW(cChecker.GetTotalPoint(sString1, sString2), invalid_argument);
+}
+
+TEST(TestCaseName, GetTotalPoint_NoStrings)
+{
+    StringSmilarityChecker cChecker;
+
+    string sString1 = "";
+    string sString2 = "";
+
+    EXPECT_THROW(cChecker.GetTotalPoint(sString1, sString2), invalid_argument);
 }
--- TDD_StringSimilarityChecker/StringSimilarityChecker.cpp
+++ TDD_StringSimilarityChecker/StringSimilarityChecker.cpp
@@ -1,4 +1,14 @@
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
 class StringSmilarityChecker
 {
 public:
+    int GetTotalPoint(string sString1, string sString2)
+    {
+        if (0 == sString1.size() || 0 == sString2.size())
+            throw invalid_argument("값을 넣어야 합니다.");
+    }
 };
